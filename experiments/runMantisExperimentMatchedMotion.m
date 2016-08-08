c6125ee9c1806a9f392a8a02607af20c6f6b0be0
Ghaith Tarawneh <gtarawneh@gmail.com>
2016-08-08 16:22:53 +0100

    Add runMantisExperimentMatchedMotion

---
+++ experiments/runMantisExperimentMatchedMotion.m
@@ -0,0 +1,215 @@
+% IMPORTANT NOTE:
+% Concerning the interpretation of the `crossed` parameter:
+%
+% the crossed field in the experiment parameters assumes that ** blue
+% filter is on left eye **
+% 
+% This means (for an upside-down mantis):
+% 
+% when blue filter is on left eye, the parameter crossed actually indicates
+% whether stimulus is crossed or not, aka: param crossed = 1 means stimulus
+% crossed param crossed = 0 means stimulus uncrossed
+% 
+% however, if blue filter is on **RIGHT** eye, the parameter crossed is
+% interpreted in the opposite way, aka: param crossed = 1 means stimulus
+% **uncrossed** param crossed = 0 means stimulus **crossed**
+%
+% Ghaith & Vivek (13/6/2016)
+
+function runMantisExperimentMatchedMotion()
+
+expt = struct;
+
+expt.genParamSetFun = @genParamSet;
+
+expt.runTrialFun = @runTrial;
+
+expt.runAfterTrialFun = @runAfterTrial;
+
+expt.runBeforeTrialFun = @(varargin) 0;
+
+expt.workDir = 'x:\readlab\Ghaith\m3\data\mantisMatchedMotion\';
+
+expt.name = 'Mantis Matched Motion';
+
+expt.recordVideos = 1;
+
+expt.makeBackup = 1;
+
+expt.defName = 'Vivek';
+
+isLarge = getDotSize();
+
+expt.addTags = {ifelse(isLarge, 'LARGE', 'SMALL')};
+
+checkBinary = @(str) (str2double(str) >= 0) && (str2double(str) <= 1);
+
+glassLeft = getNumber('Specify which filter color is on the LEFT eye (1 = green, 0 = blue): ', checkBinary);
+
+expt.addTags{end+1} = ifelse(glassLeft == 0, 'BLUE_LEFT', 'GREEN_LEFT');
+
+runExperiment(expt);
+
+end
+
+function isLarge = getDotSize()
+
+isLarge = 1; % 0 or 1
+
+end
+
+function paramSet = genParamSet()
+
+isLarge = getDotSize();
+
+blocks = 2;
+
+viewD = input('Enter distance between mantis and screen (cm): ');
+
+crossed = [0 1];
+
+conds = [1 2 3 3 4 4 5 5];
+
+% paramSet = createRandTrialBlocks(blocks, conds, crossed, viewD, isLarge);
+
+paramSet = createTrial(conds, 1, viewD, isLarge);
+
+end
+
+function runTrial(paramSetRow)
+
+disp('rendering the stimulus ...');
+
+cond = paramSetRow(1);
+
+crossed = paramSetRow(2);
+
+viewD = paramSetRow(3);
+
+isLarge = paramSetRow(4);
+
+n = ifelse(isLarge, 1e3, 1e4); % number of dots  
+
+r = ifelse(isLarge, 60, 20); % radius
+
+runDotsVAR3_wrapper(cond, crossed, viewD, n, r);
+
+end
+
+function resultRow = runAfterTrial(varargin)
+
+resultRow = 0;
+
+end
+
+%%
+
+function runDotsVAR3_wrapper(cond, crossed, viewD, n, r)
+
+delayBefore = 55;
+
+delayAfter = 5;
+
+% delayBefore = 1; delayAfter = 1; % FOR TESTING
+
+finalPresentationTime = 2;
+
+if cond == 1
+    
+    targetDirection1 = 4;
+    targetDirection2 = 6;
+    
+    stimType = 0;
+    
+elseif cond == 2
+    
+    targetDirection1 = 6;
+    targetDirection2 = 4;
+    
+    stimType = 0;
+    
+elseif cond == 3
+    
+    targetDirection1 = 6;
+    targetDirection2 = 6;
+    
+    stimType = 0;
+    
+elseif cond == 4
+    
+    targetDirection1 = 4;
+    targetDirection2 = 4;
+    
+    stimType = 0;
+    
+elseif cond == 5
+    
+    targetDirection1 = nan;
+    targetDirection2 = nan;
+    
+    stimType = 1;
+    
+else
+    
+    error('incorrect stimType');
+    
+end
+
+args = struct('bugY', 0.5, 'renderChannels', [0 1], 'v', 0, ...
+    'vTargetDots', 2, 'pairDots', 1, ...
+    'preTrialDelay', delayBefore, 'motionDuration', 0, ...
+    'finalPresentationTime', finalPresentationTime, ...
+    'targetDirection1', targetDirection1, ...
+    'targetDirection2', targetDirection2, 'viewD', viewD, ...
+    'enableKeyboard', 0, ...
+    'interTrialTime', delayAfter, ...
+    'n', n, 'r', r);
+
+if stimType
+    
+    % luminance flip
+    
+    lumFun1 = @(ch, x, y, bugX, bugY, bugRad, G, D) ...
+        getDotLum(ch, x, y, bugX, bugY, bugRad, G, D, crossed);
+    
+    args.lumFun1 = lumFun1;
+    
+    args.dispFun1 = @getDotDisp;
+    
+    args.pairDots = 0;
+    
+    runDots(args);
+    
+else
+    
+    % coherent motion
+    
+    args.crossed = crossed;
+    
+    runDotsVAR3(args);
+    
+end
+
+end
+
+function lum = getDotLum(ch, x, y, bugX, bugY, bugRad, G, D, crossed)
+
+dispSign = (-1)^ch;
+
+dispSign2 = ifelse(crossed, 1, -1);
+
+shiftX =  D/2 * dispSign * dispSign2;
+
+inBug = sqrt((x - bugX + shiftX).^2 + (y - bugY).^2) < bugRad;
+
+lum = (-1).^inBug .* ((-1).^G(:, 1));
+
+lum = lum > -1;
+
+end
+
+function disp = getDotDisp(~, x, varargin)
+
+disp = zeros(size(x));
+
+end
---
+++ stimuli/runDotsVAR3.m
@@ -0,0 +1,353 @@
+% this is a major refactoring of runDotsAnagylph
+%
+% to remove code sections that were carried over from previous experiments
+% but are no longer needed, plus introduces new flags to make it easier
+% to render new stimulus variations for future experiments
+
+% Ghaith Tarawneh (ghaith.tarawneh@ncl.ac.uk) - 1/6/2016
+
+function varargout = runDotsVAR3(args)
+
+%inDebug();
+
+% parameters
+
+videoFile = ''; % leave empty to disable recording
+
+recordingFrameRate = 60;
+
+% dot params: -------------------------------------------------------------
+
+n = 10000; % number of dots
+
+r = 20; % diameter
+
+v = 0.1  ; % velocity
+
+vTargetDots = 2  ; % velocity
+
+pairDots = 0; % when set to 0, dots in different channels become unpaired
+
+targetDirection1 = 4; % 3 = right, 4 = down, 5 = left, 6 = up
+targetDirection2 = 6; % 3 = right, 4 = down, 5 = left, 6 = up
+
+% bug params: -------------------------------------------------------------
+
+bugRadius = 0.5; % bug radius (cm) as perceived by the mantis at virtDm position
+
+virtDm = 2.5; % virtual distance from mantis (cm)
+
+bugY = 0.65;
+
+crossed = 1;
+
+% timing params: ----------------------------------------------------------
+
+preTrialDelay = 5; % delay before bug appears (seconds)
+
+motionDuration = 5; % duration of swirl (seconds)
+
+finalPresentationTime = 2; % bug visible after swirl (seconds)
+
+interTrialTime = 600; % delay after bug disappears (seconds)
+
+% screen params: ----------------------------------------------------------
+
+Gamma = 2.127; % for DELL U2413
+
+sf = 37.0370; % screen scaling factor (px/cm) for Dell U2413
+
+LeftGains = [0 0.66 0];
+
+RightGains = [0 0 1];
+
+% mantis params: ----------------------------------------------------------
+
+iod = 0.7 ; % inter-ocular distance (cm)
+
+viewD = 10; % viewing distance (cm)
+
+% control flags: ----------------------------------------------------------
+
+previewMotionFuncs = 0;
+
+enableKeyboard = 1;
+
+useRedBlue = 0;
+
+plotTarget = [];
+
+%% Unsorted parameters
+
+bgLum = 0.5; % backgrounf luminnance
+
+dotInfo = [];
+
+renderChannels = [0 1];
+
+%% parameter overrides
+
+if nargin>0; unpackStruct(args); end
+
+if nargin && (isfield(args, 'virtDm1') || isfield(args, 'virtDm1'))
+    
+    error('this stimulus no longer supports these parameters');
+        
+end
+
+%% timing calculations
+
+bugVisibleTime = motionDuration + finalPresentationTime; % duration of bug visibility(seconds)
+
+totalTime = motionDuration + finalPresentationTime + interTrialTime;
+
+%% bug size calculations
+
+% disparity:
+
+if virtDm>viewD; error('virmDm should not be larger than viewD!'); end
+
+D = calDisp(virtDm, viewD, iod) * sf; % disparity in pixels
+
+% radius:
+
+virtBugRadiusPx = bugRadius .* viewD./ virtDm * sf;
+
+%% motion function
+
+[sW, sH] = getResolution();
+
+centerX = sW * 0.5;
+
+centerY = sH * bugY;
+
+[X, Y] = getAppear(centerX, centerY);
+
+if previewMotionFuncs
+    
+    t = 0:1e-2:5; %#ok<UNRCH>
+    
+    plot(t, [X(t); Y(t)]);
+    
+    xlabel('Time (sec)');
+    
+    legend('X', 'Y');
+    
+    return
+    
+end
+
+%% create Window
+
+if useRedBlue
+    
+    RightGains = [1 0 0]; %#ok<UNRCH>
+    
+    LeftGains = [0 0 1];
+    
+end
+
+createWindow3DAnaglyph(Gamma, LeftGains, RightGains);
+
+window = getWindow();
+
+%% initial dot positions and velocities
+
+if ~isempty(dotInfo)
+    
+    unpackStruct(dotInfo);
+    
+    n = size(xs, 1); %#ok<NODEF>
+
+else
+    
+    xs = rand(n, 10) * (sW + 2 * r) - r;    
+    ys = rand(n, 10) * (sH + 2 * r) - r;
+    
+    vs = v * ones(n, 10);
+    
+    thetas = rand(n, 10) * 360;
+    
+    thetas(:, 3) = 0;
+    thetas(:, 4) = 90;
+    thetas(:, 5) = 180;
+    thetas(:, 6) = 270;
+    
+    thetas(:, 7) = 0;
+    thetas(:, 8) = 90;
+    thetas(:, 9) = 180;
+    thetas(:, 10) = 270;
+    
+    vs(:, 1:2) = v;
+    
+    vs(:, 3:end) = vTargetDots;
+    
+    dotLum = rand(n, 10) > 0.5;
+    
+    G = randb(n, 10); % random matrix to select arbitrary dot subsets
+    
+end
+
+%% rendering loop
+
+KbName('UnifyKeyNames');
+
+t0 = GetSecs();
+
+recording = recordStimulus(videoFile);
+
+endRecording = @() recordStimulus(recording);
+
+obj1 = onCleanup(endRecording);
+
+i = 0;
+
+while 1
+    
+    i = i + 1;
+    
+    tReal = GetSecs() - t0 - preTrialDelay;
+    
+    tFrames = i / recordingFrameRate - preTrialDelay;
+    
+    t = ifelse(isempty(recording), tReal, tFrames);
+    
+    if t>totalTime; break; end
+    
+    bugVisible =  t>0 && t<bugVisibleTime;
+    
+    bugX = ifelse(bugVisible, X(t), inf);
+    bugY = ifelse(bugVisible, Y(t), inf);
+    
+    % update positions
+    
+    xs = xs + vs .* cosd(thetas);    
+    ys = ys + vs .* sind(thetas);
+    
+    % reposition dots moving off the screen
+    
+    xs(xs > sW+r) = -r;
+    ys(ys > sH+r) = -r;
+    
+    xs(xs < -r) = sW + r;    
+    ys(ys < -r) = sH + r;
+    
+    % draw
+    
+    for channel = renderChannels
+        
+        % calculate dot positions:
+        
+        ch = ifelse(pairDots, 1, 1 + channel);
+        
+        ch2 = 1 + channel;
+        
+        XS1 = xs(:, ch2);
+        YS1 = ys(:, ch2);
+        
+        lum1 = dotLum(:, ch);
+        
+        targetDirection = ifelse(ch2 == 1, targetDirection1, targetDirection2);
+        
+        XS2 = xs(:, targetDirection + 4 * channel);
+        YS2 = ys(:, targetDirection + 4 * channel);
+        
+        lum2 = dotLum(:, targetDirection + 4 * channel);
+        
+        dispSign = (-1)^channel;
+        
+        dispSign2 = ifelse(crossed, 1, -1);
+        
+        shiftX = D/2 * dispSign * dispSign2;
+        
+        inBug1 = sqrt((XS1 - bugX + shiftX).^2 + (YS1 - bugY).^2) < virtBugRadiusPx;
+        
+        inBug2 = sqrt((XS2 - bugX + shiftX).^2 + (YS2 - bugY).^2) < virtBugRadiusPx;
+        
+        XS = [XS1(~inBug1); XS2(inBug2)];
+        
+        YS = [YS1(~inBug1); YS2(inBug2)];
+        
+        LUM = [lum1(~inBug1); lum2(inBug2)];
+        
+        pos = [XS YS];
+        
+        % draw:
+        
+        Screen('SelectStereoDrawBuffer', window, channel);
+        
+        Screen(window, 'FillRect', [1 1 1] * bgLum, []);
+        
+%         dotLum = lumFun1(channel, XS, YS, bugX, bugY, virtBugRadiusPx, G, D);
+        
+        Screen(window, 'DrawDots', pos', r,  (LUM * [1 1 1])' , [], 2);
+        
+        bugRect = [bugX bugY bugX bugY] + [-1 -1 1 1] * virtBugRadiusPx;
+        
+        rectL = bugRect + [1 0 1 0] * D/2;
+        rectR = bugRect + [1 0 1 0] * -D/2;
+        
+        if ismember(0, plotTarget)
+            
+            Screen(window, 'FrameOval', [1 1 1] * 0, bugRect , 4);
+            
+        end
+        
+        if ismember(-1, plotTarget)
+            
+            Screen(window, 'FrameOval', [1 1 1] * 0, rectL , 2);
+            
+        end
+        
+        if ismember(1, plotTarget)  
+            
+            Screen(window, 'FrameOval', [1 1 1] * 0, rectR , 2); 
+            
+        end
+        
+    end
+
+    Screen(window, 'Flip');
+    
+    recording = recordStimulus(recording);
+    
+    if enableKeyboard
+        
+        % handle key presses
+        
+        [~, ~, keyCode] = KbCheck;
+        
+        if (keyCode(KbName('Space'))); t0 = GetSecs(); end
+        
+        if keyCode(KbName('Escape')); break; end
+        
+        if keyCode(KbName('END')); break; end
+        
+        if keyCode(KbName('RightArrow')); 
+            
+            v = abs(v);
+            
+        end
+        
+        if keyCode(KbName('LeftArrow')); 
+            
+            v = -abs(v);
+            
+        end
+        
+    end
+    
+end
+
+dotInfo = struct('xs', xs, 'ys', ys, 'thetas', thetas, 'G', G);
+
+if nargout; varargout{1} = dotInfo; end
+
+end
+
+function [X, Y] = getAppear(centerX, centerY)
+
+X = @(t) centerX + (t<0) * 1e3;
+Y = @(t) centerY + (t<0) * 1e3;
+
+end
