6d1742dca0348d68c37a885d5020a6bc4340ae40
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-02-18 15:53:55 +0000

    Add showMessage and showSymbol

---
+++ stimuli/showMessage.m
@@ -0,0 +1,47 @@
+function showMessage(text, size)
+
+if nargin < 2
+    
+    text = 'Hello there people!';
+    
+    text = double(text);
+    
+    size = 160;
+    
+end
+
+Screen('Preference', 'TextRenderer', 1);
+
+createWindow();
+
+window = getWindow();
+
+oldSize = Screen('TextSize', window, size);
+
+oldFont = Screen('TextFont', window, 'Dingbats');
+
+% calculate positions
+
+bounds = Screen('TextBounds', window, text);
+
+% draw
+
+tW = bounds(3);
+tH = bounds(4);
+
+[sW, sH] = getResolution();
+
+tx = (sW - tW)/2;
+ty = (sH - tH)/2;
+
+Screen('DrawText', window, text, tx, ty);
+
+Screen('Flip', window);
+
+% restore previous settings
+
+Screen('TextSize', window, oldSize);
+
+Screen('TextFont', window, oldFont);
+
+end
---
+++ stimuli/showSymbol.m
@@ -0,0 +1,13 @@
+function showSymbol(symbol)
+
+    map1 = createMap({'tick', 'cross'}, {'\x2713', '\x2715'});
+    
+    code = map1.get(symbol);
+    
+    assert(~isempty(code), 'invalid symbol');
+    
+    text = double(sprintf(code));
+    
+    showMessage(text, 160);
+
+end
